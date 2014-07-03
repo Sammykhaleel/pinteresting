d5997640bbac56669fa381e6f1e43146b4d0c1b3
sammykhaleel <sammykhaleel83@gmail.com>
2014-07-03 12:02:14 -0400

    Images

--- Gemfile
+++ Gemfile
@@ -10,7 +10,8 @@ gem 'binding_of_caller'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
-
+#gem "paperclip", "~> 3.0"
+gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "about_me" =>"pages#about_me" 
+   get "pins" =>"pins#index" 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
